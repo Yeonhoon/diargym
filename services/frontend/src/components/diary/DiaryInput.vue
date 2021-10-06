b6b57afecd1de7ae8c163bc26e4f0a4572a44ea5
yeonhoon <aidenjang94@gmail.com>
2021-10-06 22:13:08 +0900

    diary만들기

--- services/frontend/src/components/diary/DiaryInput.vue
+++ services/frontend/src/components/diary/DiaryInput.vue
@@ -1,39 +1,96 @@
 <template>
-    <v-form >
-        <v-select
-            label="대분류"
-            :items="catBig"
-        ></v-select>
-        <v-select
-            label="중분류"
-            :items="catMid"
-        ></v-select>
-        <v-select
-            label="소분류"
-            :items="catSmall"
-        >
-
-        </v-select>
-        <v-text-field
-            label="무게"
-        >
-
-        </v-text-field>
-        <v-text-field
-            label="rep"
+    <v-app>
+        <v-flex>
+            <v-select
+                label="대분류"
+                outlined
+                required
+                :items="catBig"
+                v-model="big"
+            ></v-select>
+            <v-select
+                label="중분류"
+                outlined
+                required
+                :items="catMid"
+                v-model="mid"
+            ></v-select>
+            <v-select
+                label="소분류"
+                outlined
+                required
+                :items="catSmall"
+                v-model="small"
+            >
+            </v-select>
+        <v-btn @click="submit">저장</v-btn>
+        <v-btn @click="addSet">+</v-btn>
+        </v-flex>
+        <v-flex
+            v-for="(value, i) in setFields"
+            :key="i"
         >
             
-        </v-text-field>
+            <v-row>
+                <v-col cols="12" sm="1"><span>set {{i+1}}</span></v-col>
+                <v-col cols="12" sm="5">
+                    <v-text-field
+                        outlined
+                        placeholder="무게"
+                        :label="value.lab1"
+                        v-model="value.weight"
+                    >
+                    </v-text-field>
 
-    </v-form>
+                </v-col>
+                <v-col cols="12" sm="5">
+                    <v-text-field
+                        outlined
+                        placeholder="반복횟수"
+                        :label="value.lab2"
+                        v-model="value.reps"
+                    >
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="1">
+                    <v-btn @click="removeSet(i)">-</v-btn>
+                </v-col>
+            </v-row>
+            <!-- TODO: 단위 checkbox -->
+
+        </v-flex>
+    </v-app>
+   
 </template>
 <script>
 export default {
     data: () => ({
         catBig:['가슴','등','이두','삼두','하체','코어'],
         catMid:['케이블','머신','바벨','덤벨','맨몸'],
-        catSmall:[]
-    })
+        catSmall:[],
+        big:'',
+        mid:'',
+        small:'',
+        setFields:[],
+    }),
+    methods:{
+        submit(){
+            console.log(this.big)
+        },
+        addSet(){
+            this.setFields.push({
+                lab1:'weight',
+                weight:"",
+                lab2:"reps",
+                reps:""
+
+            })
+        },
+        removeSet(index){
+            this.setFields.splice(index,1)
+        }
+    }
+    
 }
 
 </script>
